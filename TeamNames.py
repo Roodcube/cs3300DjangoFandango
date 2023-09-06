1490ea2b76805974446dab0ceef6ea3aace28268
Andres Puga <andypuga@live.com>
2023-09-06 08:47:35 -0600

    "Added Andres Puga echo line"

--- TeamNames.py
+++ TeamNames.py
@@ -5,4 +5,5 @@ echo "Django Fandango"
 echo "Loe Malabanan"
 =======
 echo "Deven Rood is part of the Fandango"
+echo "Andres Puga is part of the Fandango"
 >>>>>>> 9ad695f20dffadcc6087e55471585ac5e0fe45a6
