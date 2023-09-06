d56d34cf30feaec41c72a950f61235c524e016fb
James-Starner <jstarner3@gmail.com>
2023-09-06 17:29:13 -0600

    added name to the document

--- TeamNames.py
+++ TeamNames.py
@@ -6,4 +6,5 @@ echo "Loe Malabanan"
 =======
 echo "Deven Rood is part of the Fandango"
 echo "Andres Puga is part of the Fandango"
+echo "James Starner is part of the Fandango"
 >>>>>>> 9ad695f20dffadcc6087e55471585ac5e0fe45a6
